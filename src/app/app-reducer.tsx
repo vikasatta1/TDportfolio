90e0d94cba53756c629c81ced3d5a4c309aa37f6
Vika <Vikasatta1998@gmail.com>
2022-10-05 23:11:07 +0300

    disable trash

--- src/app/app-reducer.tsx
+++ src/app/app-reducer.tsx
@@ -8,7 +8,7 @@ type StateType = {
 }
 const initialState:StateType = {
     status: '' ,
-    error: 'ERROR lalal'
+    error: null
 }
 
 /*type InitialStateType = typeof initialState*/
@@ -24,8 +24,8 @@ export const appReducer = (state: StateType = initialState, action: ActionsType)
             return state
     }
 }
-export const changeAppStatus = (status:RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
-export const appErrorAC = (error:any) => ({type: 'APP/SET-ERROR', error} as const)
+export const setAppStatusAC = (status:RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setAppErrorAC = (error:any) => ({type: 'APP/SET-ERROR', error} as const)
 type ActionsType = changeAppStatusType | errorAppType
-export type changeAppStatusType = ReturnType<typeof changeAppStatus>
-export type errorAppType = ReturnType<typeof appErrorAC>
+export type changeAppStatusType = ReturnType<typeof setAppStatusAC>
+export type errorAppType = ReturnType<typeof setAppErrorAC>
